d6f00d0d710b91fd269300e3820636f256a41d8a
0x5a455553 <49539110+0x5a455553@users.noreply.github.com>
2019-04-25 00:23:24 +0700

    Delete jquery.js

--- jquery.js
+++
@@ -1,1 +0,0 @@
-$('.y').html(atob('TUFSSUp1QU5B')).click(function(){location=window.location.pathname});const i='&#8212;';$('.q').html(`${i} DIOS ${i} NO ${i} CREA ${i} NADA ${i} EN ${i} VANO ${i}`);
